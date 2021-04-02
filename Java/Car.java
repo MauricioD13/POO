ae6cdfddb61b5e00bf9565f1f2320e29cf3a08fd
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2021-04-02 10:25:45 -0500

    Avances en la app

--- Java/Car.java
+++ Java/Car.java
@@ -1,6 +1,10 @@
 class Car {
     Integer id;
     String license;
-    String drive;
-    Integer passenger;   
+    String driver;
+    Integer passenger;  
+    
+    void printDataCar(){
+        System.out.println("License: "+ license + ", Driver: "+ driver);
+    }
 }
